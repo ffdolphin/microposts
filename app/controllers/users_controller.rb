7d5f787df0499355b4c721c111404f3184fb6fd6
Katsuhisa Takeda <katsuhisa12@hotmail.com>
2015-12-07 13:56:20 +0900

    login added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,10 +18,25 @@ class UsersController < ApplicationController
   	end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
   	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_users
+      @user = User.find(params[:id])
   end
 
 end
