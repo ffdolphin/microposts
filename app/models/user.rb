eda7aa12a6429a81f5f7016c6d83af8fa440a041
Katsuhisa Takeda <katsuhisa12@hotmail.com>
2015-12-11 21:50:40 +0900

    user model fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,6 +42,6 @@ class User < ActiveRecord::Base
 	end
 
 	def feed_items
-		Micropst.where(user_id: following_user_ids + [self.id])
+		Micropost.where(user_id: following_user_ids + [self.id])
 	end
 end
